f1f2d4a9e8762b5c2cce99f37771e8d459438897
aryyudha.project <aryyudhanugroho03@gmail.com>
2023-06-12 15:52:18 +0700

    Melengkapi Method Matematika

--- src/hitung/Matematika.java
+++ src/hitung/Matematika.java
@@ -6,11 +6,28 @@ public class Matematika {
     private double bil1, bil2;
 
     //Membuat Konstruktor
-public Matematika (double,bil1, double bil2) {
+public Matematika(double bil1, double bil2) {
     this.bil1 = bil1;
     this.bil2 = bil2;
     }
-double setPenjumlahan(){
+
+//Membuat method Setpenjumlahan
+    public double setPenjumlahan(){
     return bil1 + bil2;
+    }
+    
+    //Membuat method Setpengurangan
+    public double setPengurangan(){
+    return bil1 - bil2;
+    }
+    
+    //Membuat method SetPerkalian
+    public double setPerkalian(){
+    return bil1 * bil2;
+    }
+    
+     //Membuat method SetPembagian
+    public double setPembagian(){
+    return bil1 / bil2;
     }
 }
